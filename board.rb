5928d6e0897b437840af4fa372a7121dbb87f6ca
“sajeremy” <9jsantiago4@gmail.com>
2022-09-27 14:32:19 -0400

    2:32

--- board.rb
+++ board.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Board
 
     attr_reader :size
@@ -8,21 +9,30 @@ class Board
     end
 
     def populate
-        @grid.each do |row|
-            (row.length-1).times do 
-                row << Card.new(val)
+        arr = random_value_pairs
+        debugger
+        arr.each do |val|
+            @grid.each do |row|
+                (row.length-1).times do 
+                    debugger
+                    row << Card.new(val)
+                end
             end
         end
+        @grid
+        #debugger
     end
 
-    def random_value_pairs(size)
-        unique_char_count = size/2
+    def random_value_pairs
+        unique_char_count = (size**2)/2
         alphabet = ("a".."z").to_a
         values = alphabet.sample(unique_char_count)
         value_pairs = values.concat(values)
-        values_pairs.sample(value_pairs.length)
+        value_pairs.sample(value_pairs.length)
     end
     
-
-
 end
+
+board = Board.new
+board.populate
+board.random_value_pairs
